69d47a3914a9c75983c73cbcfa33478add847237
Alex Altn <alex.altn@gmail.com>
2020-03-06 15:24:24 +0200

    react app - адаптация для моб. экранов

---
+++ app/precache-manifest.37156ee3876d1914d33802bd2f5ad0f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d679dca01491cf6fc084987cd10e7550",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "8115c7ee74d72c1dcb9e",
+    "url": "/app/static/css/main.cb9a8eb6.chunk.css"
+  },
+  {
+    "revision": "10dd6751ed25cb474ea7",
+    "url": "/app/static/js/2.871aca67.chunk.js"
+  },
+  {
+    "revision": "8115c7ee74d72c1dcb9e",
+    "url": "/app/static/js/main.55c9ce19.chunk.js"
+  },
+  {
+    "revision": "2545e4f837a115bcd8be",
+    "url": "/app/static/js/runtime-main.c9d2aecf.js"
+  }
+]);
